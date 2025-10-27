8ce5547f9ab1b3ef5f10628ec236264b3b0b93a2
JoPlusPlus <yosefhany99@gmail.com>
2025-10-27 15:54:26 +0300

    Added Linear and Logistic Regression

---
+++ BLR_M.py
@@ -0,0 +1,46 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
+from sklearn import datasets
+from sklearn.preprocessing import StandardScaler
+
+
+X,Y = datasets.load_breast_cancer(return_X_y=True)
+x_train,x_test,y_train,y_test = train_test_split(X,Y,test_size=0.2,random_state=1234)
+
+scaler = StandardScaler()
+x_train = scaler.fit_transform(x_train)
+x_test = scaler.fit_transform(x_test)
+w = np.zeros(30)
+b = 0
+def sigmoid(z):
+    return 1.0 / (1.0 + np.exp(-z))
+
+def costFunction(x,y,w,b,LAMBDA):
+    m = len(x)
+    f_wb = sigmoid(x.dot(w)+b)
+    return (1/m) * np.sum( (-y * np.log(f_wb)) - ( (1 - y) * np.log(1 - f_wb)) ) + (LAMBDA/(2*m))*np.sum(w**2)
+
+
+def fit(x,y,w,b,alpha,LAMBDA,iter):
+    m = len(x)
+    for i in range(iter):
+        dw = (1/m) * np.dot((sigmoid(x.dot(w) + b)-y),x) + (LAMBDA/m)*w
+        db = (1/m) * np.sum((sigmoid(x.dot(w) + b)-y))
+
+        w = w - alpha * dw
+        b = b - alpha * db
+    return w,b
+def predict(x,w,b):
+    z = sigmoid(x.dot(w) + b)
+    z = [0 if i < 0.5 else 1 for i in z]
+    return z
+
+w,b = fit(x_train,y_train,w,b,0.01,1,iter=1000)
+
+x_train_pred = predict(x_train,w,b)
+
+acc = accuracy_score(y_train,x_train_pred)
+print(f"{acc*100}%")
+
---
+++ Multiple LinearR.py
@@ -0,0 +1,54 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+data = pd.read_csv("multiple_linear_regression_dataset.csv")
+x = data[["age","experience"]].to_numpy()
+y = data["income"].to_numpy()
+
+y = y/1000
+
+x_train = x[:15]
+y_train = y[:15]
+x_test = x[15:]
+y_test = y[15:]
+
+
+def model(x,w,b):
+    return x.dot(w) + b
+def costFunction(x,y,w,b,m):
+    yhat = model(x,w,b)
+    return (1/(2*m)) * np.sum((yhat - y)**2)
+def gradientDescent(x,y,w,b,alpha,iterations,m):
+    costList = []
+
+    for i in range(iterations):
+        yhat = model(x,w,b)
+        error = yhat - y
+        dw = (1/m) * (np.sum(error[:,None] * x, axis=0))
+        db = (1/m) * np.sum(error)
+
+        cost = costFunction(x,y,w,b,m)
+        costList.append(cost)
+        if i % 100 == 0:
+            print(f"Iteration {i} : Cost = {cost}")
+
+        w -= alpha * dw
+        b -= alpha * db
+    return w,b,costList
+
+m = len(x_train)
+alpha = 0.001
+iterations = 100000
+w = np.array([0.0 , 0.0])
+b = 0
+
+w,b,CostHistory = gradientDescent(x_train,y_train,w,b,alpha, iterations, m)
+print("---------------")
+print(f"Final w = {w}")
+print(f"Final bias = {b}")
+print(f"Final cost = {CostHistory[-1]}")
+
+plt.scatter(x[:, 0],y,c="red",label = "Actual")
+plt.scatter(x[:, 0],model(x,w,b), c="green", label="Predicted", marker="x")
+plt.show()
